724ebfab51541ca957979106f0b2f8c0b5526959
Freesia810 <z1402121306@hotmail.com>
2023-05-06 11:42:55 +0800

    modified:   src/main/resources/metatrade-gateway.properties 	modified:   src/test/java/com/freesia/metatradegateway/CryptoUtilsTests.java

--- src/test/java/com/freesia/metatradegateway/CryptoUtilsTests.java
+++ src/test/java/com/freesia/metatradegateway/CryptoUtilsTests.java
@@ -1,2 +1,12 @@
-package com.freesia.metatradegateway;public class CryptoUtilsTest {
+package com.freesia.metatradegateway;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CryptoUtilsTests {
+    @Test
+    public void TestSha256(){
+        var ret = CryptoUtils.getSHA256("123");
+        Assertions.assertEquals(ret, "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3");
+    }
 }
