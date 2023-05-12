cb4068305726fa521106c8d257cfbefeb2a3ead7
Freesia810 <z1402121306@hotmail.com>
2023-05-12 23:48:55 +0800

    modified:   src/main/java/com/freesia/metatradegateway/CryptoUtils.java

--- src/main/java/com/freesia/metatradegateway/CryptoUtils.java
+++ src/main/java/com/freesia/metatradegateway/CryptoUtils.java
@@ -18,16 +18,6 @@ public class CryptoUtils {
         return encodeStr;
     }
 
-    public static byte[] hex2Byte(String s) {
-        int len = s.length();
-        byte[] data = new byte[len / 2];
-        for (int i = 0; i < len; i += 2) {
-            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-                                 + Character.digit(s.charAt(i+1), 16));
-        }
-        return data;
-    }
-
     private static String byte2Hex(byte[] bytes) {
         StringBuilder stringBuffer = new StringBuilder();
         String temp;
